9cb7570ea96643265cfe23e32b2bbaec873ba385
WinnerOK <danman339@gmial.com>
2021-05-08 14:12:54 +0300

    Unwrap game activity from IO

--- src/Game.hs
+++ src/Game.hs
@@ -1,28 +1,26 @@
-
-
 {-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+
 module Game where
 
-import World
 import CodeWorld
-import WorldGen
 import CodeWorldRenderer
 import Player
 import System.IO.Unsafe (unsafePerformIO)
-
+import World
+import WorldGen
 
 gameMain :: IO ()
 gameMain = do
   world <- genWorld
   -- Update the world once to generate more chunks around player
   world <- updateChunks ((pos . player) world) world
-  activityOf world (\e w -> unsafePerformIO (handleInput e w)) (unsafePerformIO . drawWorld)
+  activityOf world handleInput drawWorld
 
-drawWorld :: World -> IO Picture
+drawWorld :: World -> Picture
 drawWorld world = draw (time world) world world
 
-handleInput :: Event -> World -> IO World
-handleInput (TimePassing t) w = return $ w { time = t }
-handleInput e w = do
-  newPlayer <- move e w (player w)
-  return $ w { player = newPlayer }
+handleInput :: Event -> World -> World
+handleInput (TimePassing t) w = w {time = t}
+handleInput e w = w {player = newPlayer}
+  where
+    newPlayer = move e w (player w)
