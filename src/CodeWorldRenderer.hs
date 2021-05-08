14d3bbe9e06a6c32c3cac3533487eddae8e9a00a
WinnerOK <danman339@gmial.com>
2021-05-08 14:37:41 +0300

    Remove color from void

--- src/CodeWorldRenderer.hs
+++ src/CodeWorldRenderer.hs
@@ -10,8 +10,6 @@ import World
 
 renderRange = 20
 
-voidColor = RGB 0.1 0.1 0.1
-
 -- | Defines a renderable object.
 class Drawable a where
   -- | renders a tile based on current time and world state. No transformations
@@ -22,7 +20,6 @@ translate :: Coords -> Picture -> Picture
 translate (cx, cy) = translated (fromIntegral cx) (fromIntegral cy)
 
 instance Drawable Tile where
-  -- draw t w Void = return $ colored voidColor (solidRectangle 1 1)
   draw t w Void = blank
   draw t w (Ground color) = colored color (solidRectangle 1 1)
   draw t w (Wall color) = colored color (solidRectangle 0.9 0.9)
