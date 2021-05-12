d49560f19b3c9d01471f765c6a935faa326d1972
IamMaxim <g.maxim.stepanoff@gmail.com>
2021-05-12 19:57:09 +0300

    Some working Perlin noise

--- src/WorldGen.hs
+++ src/WorldGen.hs
@@ -4,14 +4,15 @@
 module WorldGen where
 
 import CodeWorld hiding (Vector)
+import CodeWorldRenderer
 import Control.Monad
-import Data.Vector
 import Data.Map
+import Data.Maybe
+import Data.Vector
+import Inventory
+import Math.Noise
 import System.Random
 import World
-import CodeWorldRenderer
-
-import Inventory
 
 -- | Generates the initial world state
 genWorld :: IO World
@@ -33,14 +34,14 @@ genWorld = do
 
 -- | Generates a (for now) stub chunk of ground tiles.
 genChunk :: Coords -> IO Chunk
-genChunk _ = do
-  r <- randomIO :: IO Double
-  g <- randomIO :: IO Double
-  b <- randomIO :: IO Double
-  let color = RGB r g b
+genChunk chunkCoords = do
+  -- r <- randomIO :: IO Double
+  -- g <- randomIO :: IO Double
+  -- b <- randomIO :: IO Double
+  -- let color = RGB r g b
   return
     Chunk
-      { backgroundTiles = genTilesArray chunkSize (Ground color),
+      { backgroundTiles = perlinizeChunk chunkCoords (genTilesArray chunkSize Void),
         tiles = genTilesArray chunkSize Void
       }
 
@@ -49,7 +50,6 @@ genTilesArray n t = Data.Vector.replicate n (Data.Vector.replicate n t)
 
 -- | Generates additional chunks as needed if there are not enough of them in the player range
 updateChunks :: Coords -> World -> IO World
-
 updateChunks coords world =
   Control.Monad.foldM update world chunksToCheck
   where
@@ -71,3 +71,39 @@ updateChunks coords world =
       Nothing -> do
         chunk <- genChunk coords
         return world {chunks = insert coords chunk (chunks world)}
+
+--------------------------------------------------------------------------------
+-- Perlin stuff
+--------------------------------------------------------------------------------
+
+-- | Returns the Perlin noise generator with predefined parameters
+perlinObj ::
+  -- | seed to generate noise from
+  Int ->
+  Perlin
+perlinObj = Perlin 0.1 1 14 1
+
+-- | Generates a noise value for the given coords.
+noiseAt :: Coords -> Double
+noiseAt (x, y) =
+  case getNoiseValue (perlinObj 123) [] (fromIntegral x, fromIntegral y, 0) of
+    Nothing -> -1
+    Just d -> d
+
+perlinizeChunk :: Coords -> Vector (Vector Tile) -> Vector (Vector Tile)
+perlinizeChunk (cx, cy) tiles =
+  Data.Vector.map
+    ( \(y, ys) ->
+        Data.Vector.map
+          ( \(x, tile) ->
+              Ground (RGB (noiseAt (cx * chunkSize + x, cy * chunkSize + y)) 0 0)
+          )
+          ys
+    )
+    (indexedTiles tiles)
+
+indexedTiles :: Vector (Vector Tile) -> Vector (Int, Vector (Int, Tile))
+indexedTiles = indexed . Data.Vector.map indexed
+
+deindexedTiles :: Vector (Int, Vector (Int, Tile)) -> Vector (Vector Tile)
+deindexedTiles = Data.Vector.map (\(x, xs) -> Data.Vector.map snd xs)
